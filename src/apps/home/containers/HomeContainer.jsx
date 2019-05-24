63f44fb36007034e99cd025c3c9920f9e020b6ef
HoshinoTouko <oranly@hotmail.com>
2019-05-24 15:53:02 +0800

    Init home page.

--- src/apps/home/containers/HomeContainer.jsx
+++ src/apps/home/containers/HomeContainer.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import {Col, Row} from 'antd';
 
 import '../style/Home.less';
+import PlayerComponent from '../components/PlayerComponent';
 
 class HomeContainer extends React.Component {
   render() {
     return (
       <Row className="home" gutter={20}>
-        Welcome home
+        <Col sm={{span: 18, push: 3}}>
+          <PlayerComponent />
+        </Col>
       </Row>
     );
   }
